12399b44af66e41b3c55fc4b8e5b700410b609f3
awp-finanznachrichten <sw@awp.ch>
2021-06-06 11:08:09 +0200

    commit from Rstudio

--- em_toto_run.R
+++ em_toto_run.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(stringr)
 library(git2r)
 library(DatawRappr)
+library(readxl)
 
 #Working Directory definieren
 setwd("C:/Users/simon/OneDrive/Fussballdaten/em-toto-2021")
@@ -10,9 +11,73 @@ setwd("C:/Users/simon/OneDrive/Fussballdaten/em-toto-2021")
 #Funktionen einlesen
 source("functions.R", encoding = "UTF-8")
 
+#Daten laden
+tipps <- read_excel("Tipps/EM-Toto 2021 (Antworten).xlsx")
+
+#Neues Dataframe erstellen
+zwischenstand_dw <- data.frame("Spieler","Kampfname",99,"Europameister-Tipp","Wie weit kommt die Schweiz?")
+colnames(zwischenstand_dw) <- c("Spieler","Kampfname","Punkte","Weltmeister-Tipp","Wie weit kommt die Schweiz?")
+
+###Eintrag für jeden Spieler
+for (p in 2:nrow(tipps)) {
+
+###Punkte evaluieren für jedes Spiel
+score <- 0
+
+for (g in 5:40) {
+
+if ( is.na(tipps[1,g]) == FALSE ) {
+
+result_home <- as.numeric(trimws(str_split(tipps[1,g],":")[[1]][1]))
+result_away <- as.numeric(trimws(str_split(tipps[1,g],":")[[1]][2]))
+guess_home <- as.numeric(trimws(str_split(tipps[p,g],":")[[1]][1]))
+guess_away <- as.numeric(trimws(str_split(tipps[p,g],":")[[1]][2]))
+
+if ( (result_home == guess_home) & (result_away == guess_away) ) {
+  
+score <- score + 3  
+  
+} else if ( (result_home > result_away) & (guess_home > guess_away) ) {
+
+score <- score + 1    
+
+} else if ( (result_home < result_away) & (guess_home < guess_away) ) {
+  
+score <- score + 1 
+
+} else if ( (result_home == result_away) & (guess_home == guess_away) ) {
+  
+  score <- score + 1 
+  
+}  
+
+
+}
+
+    
+}  
+  
+#Data entry
+player_data <- data.frame(tipps[p,3],tipps[p,4],score,tipps[p,41],tipps[p,42])
+colnames(player_data) <- c("Spieler","Kampfname","Punkte","Weltmeister-Tipp","Wie weit kommt die Schweiz?")
+
+zwischenstand_dw <- rbind(zwischenstand_dw,player_data)
+
+}    
+
+
+#Daten bearbeiten und speichern
+zwischenstand_dw <- zwischenstand_dw[-1,]
+zwischenstand_dw <- zwischenstand_dw[order(-zwischenstand_dw$Punkte),]
+write.csv(zwischenstand_dw,"Output/zwischenstand_dw.csv", na = "", row.names = FALSE, fileEncoding = "UTF-8")
+
+
 #Make Commit
+git2r::config(user.name = "awp-finanznachrichten",user.email = "sw@awp.ch")
 token <- read.csv("C:/Users/simon/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
 git2r::cred_token(token)
 gitadd()
 gitcommit()
 gitpush()
+
+#Datawrapper-Grafik veröffentlichen
